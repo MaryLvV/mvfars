d40b2da7b170d3ab5cc056f7dfc5157d758714cd
Mary van Valkenburg <maryvanvalkenburg@gmail.com>
2017-04-02 11:54:31 -0500

    adding documentation files and correcting test

--- tests/testthat/test-data-load.R
+++ tests/testthat/test-data-load.R
@@ -1,6 +1,6 @@
 #test to check that packaged dataset for 2013 loads
 
 test_that("data loads correctly",  {
-    expect_equal((nrow(fars_read("inst/extdata/accident_2013.csv.bz2")) == 30202), expected = TRUE)
-    expect_equal((ncol(fars_read("inst/extdata/accident_2013.csv.bz2")) == 50), expected = TRUE)
+    expect_equal(nrow(fars_read("inst/extdata/accident_2013.csv.bz2")),  30202)
+    expect_equal(ncol(fars_read("inst/extdata/accident_2013.csv.bz2")), 50)
 })
