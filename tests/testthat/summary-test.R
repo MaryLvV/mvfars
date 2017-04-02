ffbf0e1d0201efd36a036a089878355120dacdb6
Mary van Valkenburg <maryvanvalkenburg@gmail.com>
2017-04-02 11:04:01 -0500

    adding travis, config files

--- tests/testthat/summary-test.R
+++
@@ -1,7 +0,0 @@
-system.file("extdata", "accident_2013.csv.bz2", package = "mvfars")
-system.file("extdata", "accident_2014.csv.bz2", package = "mvfars")
-system.file("extdata", "accident_2015.csv.bz2", package = "mvfars")
-
-
-
-fars_summarize_years(2015)
